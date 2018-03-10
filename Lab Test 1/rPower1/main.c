4c71c05c00440d09191064faea7e11e621316ce3
l0rem1psum <sunshine.xuan5960@gmail.com>
2018-03-11 03:01:32 +0800

    Update main.c

--- Lab Test 1/rPower1/main.c
+++ Lab Test 1/rPower1/main.c
@@ -16,7 +16,7 @@ float rPower1(float num, int p)
     if (p == 0){
         return 1;
     } else {
-        if (p < 0) {return 1 / num * rPower1(1/num, (0 - p) - 1);}
+        if (p < 0) {return 1 / num * rPower1(1 / num, (0 - p) - 1);}
         else {return num * rPower1(num, p - 1);}
     }
 }
